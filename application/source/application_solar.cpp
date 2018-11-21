b93eea07c3940679b2872db0424aacde6b2c6baf
dudo1489 <sebastian.heckner@gmx.de>
2018-11-21 21:06:06 +0100

    application solar update

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -416,14 +416,14 @@ void ApplicationSolar::renderObjects() const{
     model_matrix = glm::rotate(model_matrix, float(glfwGetTime()), glm::fvec3{0.0f, 1.0f, 0.0f});
     glm::fvec3 scale {(9-i)/3, (9-i)/3, (9-i)/3};
     model_matrix = glm::scale(model_matrix, scale);
-    glm::fvec3 planet_color;
-    if(i==0){
-      planet_color= {0.1,1.0,0.0};
+    glm::fvec3 planet_color {1-i/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1),0.2,i/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1)};
+    /*if(i==0){
+      planet_color = {0.1,1.0,0.0};
     }else{
-      //glm::fvec3 planet_color= {0.2,1.0*i/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1),(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1-i)/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1)};
-      planet_color= {0.2,1.0,1.0};
+      planet_color = {1-i/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1),0.2,i/(m_scene.getRoot()->getChildren("sun")->getChildrenList().size()-1)};
+      //planet_color= {0.2,1.0,1.0};
     }
-
+  */
     glUniformMatrix4fv(m_shaders.at(m_shader_name).u_locs.at("ModelMatrix"), 1, GL_FALSE, glm::value_ptr(model_matrix));
     glUniform3f(m_shaders.at(m_shader_name).u_locs.at("Planet_Color"), planet_color.x, planet_color.y, planet_color.z);
     // extra matrix for normal transformation to keep them orthogonal to surface
